746b1702090c1e67953690ea5c27c6c9b79b3187
orion28k <160691449+orion28k@users.noreply.github.com>
2025-08-25 16:51:49 -0400

    initial commit

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace ClassLibrary1;
+public class Class1
+{
+    public static string passCheck(string password)
+    {
+        int grade = 0;
+
+        if (password.Any(char.IsUpper))
+            grade++;
+        if (password.Any(char.IsLower))
+            grade++;
+        if (password.Any(char.IsDigit))
+            grade++;
+        if (password.Any(char.IsSymbol))
+            grade++;
+
+        switch (grade)
+        {
+            case 0:
+                return "INELIGABLE";
+            case 1:
+                return "WEAK";
+            case 2:
+                return "MEDIUM";
+            case 3:
+                return "MEDIUM";
+            case 4:
+                return "STRONG";
+            default:
+                return "None";
+        }
+    }
+}
